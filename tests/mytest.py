2e334fa4be9d55852ff8c9c4e34d2d7992df5f71
liying2008 <liruoer2008@yeah.net>
2023-08-06 22:50:04 +0800

    test: fix error of output dir does not exist

--- tests/mytest.py
+++ tests/mytest.py
@@ -2,12 +2,14 @@
 import os
 import unittest
 
-from tests.common import TESTCASES_DIR
+from tests.common import TESTCASES_DIR, OUTPUT_PATH
 
 
 class MyTest(unittest.TestCase):
     def setUp(self):
         os.chdir('..')
+        if not os.path.exists(OUTPUT_PATH):
+            os.makedirs(OUTPUT_PATH)
 
     def tearDown(self):
         os.chdir(TESTCASES_DIR)
