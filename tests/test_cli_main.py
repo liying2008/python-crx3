d2fe42e8c639d91408895893d528635b0451bc3e
liying2008 <liruoer2008@yeah.net>
2023-08-19 15:13:23 +0800

    test: remove shell=True from subprocess run params

--- tests/test_cli_main.py
+++ tests/test_cli_main.py
@@ -11,7 +11,7 @@ class CliMainTestCase(MyTest):
         source_path = os.path.abspath(os.path.join('example', 'example-extension'))
         output_path = os.path.abspath(os.path.join('output', 'example-extension.crx'))
         cmd = ('crx3', 'create', source_path, '-o', output_path)
-        process = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+        process = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                                  universal_newlines=True, timeout=120)
         if self.verbose:
             print('\nargs: {}'.format(' '.join(process.args)))
@@ -24,7 +24,7 @@ class CliMainTestCase(MyTest):
 
         crx_path_ok = os.path.abspath(os.path.join('example', 'example-extension.crx'))
         cmd = ('crx3', 'verify', crx_path_ok, '-v')
-        process = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+        process = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                                  universal_newlines=True, timeout=120)
         if self.verbose:
             print('\nargs: {}'.format(' '.join(process.args)))
@@ -34,7 +34,7 @@ class CliMainTestCase(MyTest):
 
         crx_path_verification_error = os.path.abspath(os.path.join(TESTCASES_DIR, 'testdata', 'verification-error.crx'))
         cmd = ('crx3', 'verify', crx_path_verification_error)
-        process = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+        process = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                                  universal_newlines=True, timeout=120)
         if self.verbose:
             print('\nargs: {}'.format(' '.join(process.args)))
@@ -45,7 +45,7 @@ class CliMainTestCase(MyTest):
         crx_path_version_number_error = os.path.abspath(
             os.path.join(TESTCASES_DIR, 'testdata', 'version-number-error.crx'))
         cmd = ('crx3', 'verify', crx_path_version_number_error)
-        process = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+        process = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                                  universal_newlines=True, timeout=120)
         if self.verbose:
             print('\nargs: {}'.format(' '.join(process.args)))
