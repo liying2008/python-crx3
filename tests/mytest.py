b24259b958f189cb86b96138ffa523b96af56488
liying2008 <liruoer2008@yeah.net>
2023-08-19 14:24:48 +0800

    test: update cli main testcase

--- tests/mytest.py
+++ tests/mytest.py
@@ -7,6 +7,11 @@ from tests.common import TESTCASES_DIR
 
 class MyTest(unittest.TestCase):
     def setUp(self):
+        testcase_verbose = os.getenv('CRX3_TESTCASE_VERBOSE')
+        if testcase_verbose == 'true' or testcase_verbose == '1':
+            self.verbose = True
+        else:
+            self.verbose = False
         os.chdir('..')
 
     def tearDown(self):
--- tests/test_cli_main.py
+++ tests/test_cli_main.py
@@ -1,4 +1,5 @@
 import os.path
+import subprocess
 import unittest
 
 from tests.common import TESTCASES_DIR
@@ -9,28 +10,48 @@ class CliMainTestCase(MyTest):
     def test_01_main(self):
         source_path = os.path.abspath(os.path.join('example', 'example-extension'))
         output_path = os.path.abspath(os.path.join('output', 'example-extension.crx'))
-        cmd = 'crx3 create {} -o {}'.format(source_path, output_path)
-        ret = os.system(cmd)
-        self.assertEqual(0, ret)
+        cmd = ('crx3', 'create', source_path, '-o', output_path)
+        process = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                                 universal_newlines=True, timeout=10)
+        if self.verbose:
+            print('\nargs: {}'.format(' '.join(process.args)))
+            print('stdout: {}'.format(process.stdout))
+            print('stderr: {}'.format(process.stderr))
+        self.assertEqual(0, process.returncode)
         private_key_path = os.path.abspath(os.path.join('output', 'example-extension.pem'))
         self.assertTrue(os.path.exists(output_path))
         self.assertTrue(os.path.exists(private_key_path))
 
         crx_path_ok = os.path.abspath(os.path.join('example', 'example-extension.crx'))
-        cmd = 'crx3 verify {}'.format(crx_path_ok)
-        ret = os.system(cmd)
-        self.assertEqual(0, ret)
+        cmd = ('crx3', 'verify', crx_path_ok, '-v')
+        process = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                                 universal_newlines=True, timeout=10)
+        if self.verbose:
+            print('\nargs: {}'.format(' '.join(process.args)))
+            print('stdout: {}'.format(process.stdout))
+            print('stderr: {}'.format(process.stderr))
+        self.assertEqual(0, process.returncode)
 
         crx_path_verification_error = os.path.abspath(os.path.join(TESTCASES_DIR, 'testdata', 'verification-error.crx'))
-        cmd = 'crx3 verify {}'.format(crx_path_verification_error)
-        ret = os.system(cmd)
-        self.assertEqual(6, ret)
+        cmd = ('crx3', 'verify', crx_path_verification_error)
+        process = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                                 universal_newlines=True, timeout=10)
+        if self.verbose:
+            print('\nargs: {}'.format(' '.join(process.args)))
+            print('stdout: {}'.format(process.stdout))
+            print('stderr: {}'.format(process.stderr))
+        self.assertEqual(6, process.returncode)
 
         crx_path_version_number_error = os.path.abspath(
             os.path.join(TESTCASES_DIR, 'testdata', 'version-number-error.crx'))
-        cmd = 'crx3 verify {}'.format(crx_path_version_number_error)
-        ret = os.system(cmd)
-        self.assertEqual(2, ret)
+        cmd = ('crx3', 'verify', crx_path_version_number_error)
+        process = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                                 universal_newlines=True, timeout=10)
+        if self.verbose:
+            print('\nargs: {}'.format(' '.join(process.args)))
+            print('stdout: {}'.format(process.stdout))
+            print('stderr: {}'.format(process.stderr))
+        self.assertEqual(2, process.returncode)
 
 
 if __name__ == '__main__':
